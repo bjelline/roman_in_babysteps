4c5dd81281d60197013950204fc6b53238e71fc7
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2014-03-15 12:39:34 +0100

    convert 6 to VI

--- roman-and-spec.js
+++ roman-and-spec.js
@@ -1,11 +1,23 @@
-require('jasmine-matchers');
 
-it ( 'should convert 1 => ', function(){
-
-  expect( convert(1).toBe('I') );
-  
+describe("arabic to roman numerals converter", function() {
+  it ( 'should convert 1,5,6 => I,V,IV', function(){
+    expect(convert(1)).toBe('I');
+    expect(convert(5)).toBe('V');
+    expect(convert(6)).toBe('VI');
+  });
 });
 
 var convert = function(arabic) {
-  return "I";
+
+  var roman = "";
+
+  if( Math.floor(arabic/5) == 1 ) {
+    roman += "V";
+  }
+
+  if( arabic % 5 == 1 ) {
+    roman += "I";
+  }
+
+  return roman;
 }
